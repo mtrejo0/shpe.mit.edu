f186e1929c44056d3ec33db2f6639a8d7fb9ff4a
Gonzo <dggsax@gmail.com>
2018-01-05 10:45:16 -0600

    initial commit of files

---
+++ static/js/page.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        scrollingSpeed: 800,
+        menu: '#menu',
+        navigation: true,
+        anchors: ['firstpage', 'aboutme', 'thirdpage'],
+    });
+});
