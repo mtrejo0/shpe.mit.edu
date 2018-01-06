613474ca951fccf76c3a2d97c495b0d1e597a187
Gonzo <dggsax@gmail.com>
2018-01-05 21:09:57 -0600

    um

--- static/js/page.js
+++ static/js/page.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         scrollingSpeed: 800,
         menu: '#menu',
-        navigation: true,
-        anchors: ['firstpage', 'aboutme', 'thirdpage'],
+        // navigation: true,
+        // anchors: ['firstpage', 'aboutme', 'thirdpage'],
     });
+
+    createOfficerTabs();
+    
 });
