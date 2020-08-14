e5d7deccd84fbf6922ba6540ff6783546c966004
hidaiolivas <hidai.olivas@gmail.com>
2020-08-13 19:04:02 -0600

    updated logo and added insta

--- src/containers/ContactUs.jsx
+++ src/containers/ContactUs.jsx
@@ -8,7 +8,7 @@ export default class Header extends Component {
             <div className="section">
                 <h3>Contact us!</h3>
                 <p>We love connecting with members of the community who want to do work with us or with companies who'd like to learn more about opportunities connecting with SHPE and our talented pool of members directly. For all inquiries, please email <a href="mailto:shpe-exec@mit.edu?subject=Hello%20SHPE-MIT!">shpe-exec@mit.edu</a>!</p>
-                <p>You can also find us on the <a href="https://www.facebook.com/shpemit" target="_blank" rel="noopener noreferrer">Facebook</a> and <a href="https://www.linkedin.com/groups/5182958/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                <p>You can also find us on the <a href="https://www.facebook.com/shpemit" target="_blank" rel="noopener noreferrer">Facebook</a>, <a href="https://www.instagram.com/shpemit/" target="_blank" rel="noopener noreferrer">Instagram</a> and <a href="https://www.linkedin.com/groups/5182958/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
             </div>
         )
     }
